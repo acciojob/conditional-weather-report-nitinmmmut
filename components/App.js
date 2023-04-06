294ea4f1ef235aaecae18dfd7f331102a20cb151
nitinmmmut <NITINRAJ827@GMAIL.COM>
2023-04-06 19:54:38 +0530

    solved

---
+++ components/App.js
@@ -0,0 +1,22 @@
+
+import React, { useState, useEffect } from "react";
+import WeatherDisplay from "./WeatherDisplay";
+
+const App = () => {
+  const [weatherData, setWeatherData] = useState({
+    temperature: 0,
+    conditions: ""
+  });
+
+  useEffect(() => {
+    setWeatherData({ temperature: 25, conditions: "Sunny" });
+  }, []);
+
+  return (
+    <div>
+      <WeatherDisplay weatherData={weatherData} />
+    </div>
+  );
+};
+
+export default App;
